b88980dd46d0587ba653d90c013a99a7a543a60a
Khan Asfi Reza <khanasfireza10@gmail.com>
2022-07-02 02:07:28 +0600

    :sparkles: Route guard loaded added

--- src/layout/RouteGuard.tsx
+++ src/layout/RouteGuard.tsx
@@ -1,58 +1,30 @@
 import { useEffect } from "react";
 import { useRouter } from "next/router";
 import { useAuth } from "../context/AuthContext";
-import { Center, Spinner, Stack, Text, Box } from "@chakra-ui/react";
-import LogoWithText from "@images/LogoText.png";
-import Image from "next/image";
-const RouteProtect = ({ children }) => {
+import { Loader } from "@component/Loader";
+
+export const RouteUnAuthProtect = ({ children }) => {
+  const { authUser } = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (authUser) {
+      router.push("/").then().catch();
+    }
+  }, [authUser, router]);
+
+  if (!authUser) return <>{children}</>;
+};
+export const RouteAuthProtect = ({ children }) => {
   const { authUser, loading } = useAuth();
   const router = useRouter();
 
   useEffect(() => {
-    if (!loading && !authUser) router.push("/login").then((r) => {});
-  }, [authUser, loading, router]);
+    if (!loading.authLoading && !authUser) router.push("/login").then();
+  }, [authUser, loading.authLoading, router]);
 
-  if (loading || !authUser) {
-    return (
-      <Center
-        minH={"100vh"}
-        width={"100%"}
-        bg={"#f7f7f7"}
-        borderRadius={"10px"}
-      >
-        <Stack spacing={4} padding={"2rem 3rem"} bg={"white"}>
-          <Center>
-            <Box position={"relative"} height={"100px"} width={"200px"}>
-              <Image
-                alt={"Logo"}
-                layout={"fill"}
-                objectFit={"contain"}
-                src={LogoWithText}
-              />
-            </Box>
-          </Center>
-          <Center>
-            <Spinner
-              thickness="4px"
-              speed="0.65s"
-              emptyColor="gray.200"
-              color="blue.500"
-              size="xl"
-            />
-          </Center>
-          <Text
-            color={"blue.900"}
-            fontWeight={"semibold"}
-            fontSize={"xl"}
-            textAlign={"center"}
-          >
-            Just a few moments, we are almost there
-          </Text>
-        </Stack>
-      </Center>
-    );
+  if (loading.authLoading || !authUser) {
+    return <Loader />;
   }
   return <>{children}</>;
 };
-
-export default RouteProtect;
