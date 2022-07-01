f34420994e7c44a8bc4cf40c2b0c5ba5a3fdf642
Khan Asfi Reza <khanasfireza10@gmail.com>
2022-07-02 02:06:08 +0600

    :sparkles: Sidebar to have more features

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,80 +2,219 @@ import {
   Avatar,
   Button,
   Flex,
+  FormControl,
+  FormLabel,
   IconButton,
   Input,
   InputGroup,
-  InputLeftAddon,
+  InputLeftElement,
   Menu,
   MenuButton,
-  MenuList,
-  MenuItem,
-  MenuGroup,
   MenuDivider,
+  MenuGroup,
+  MenuItem,
+  MenuList,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
   Stack,
-  Box,
-  InputLeftElement,
+  Text,
+  useDisclosure,
+  useToast,
 } from "@chakra-ui/react";
-import styled from "@emotion/styled";
 import {
   IoChatboxEllipsesOutline,
   IoEllipsisVerticalOutline,
   IoSearchOutline,
 } from "react-icons/io5";
+import { useAuth, useUserDetails } from "../context/AuthContext";
+import { useState } from "react";
+import { chatCollection, userCollection } from "@db/collections";
+import { useCollection } from "react-firebase-hooks/firestore";
+import Contact from "@component/Contact";
 
-// Sidebar header | Top head content
-const Header = styled(Flex)`
-  position: sticky;
-  top: 0;
-  background: #fff;
-  z-index: 1;
-  justify-content: space-between;
-  align-items: center;
-  padding: 0.8rem 2.5rem;
-  border-bottom: 1px solid #f7f7f7;
-`;
+const range = (n) => {
+  return Array.from(Array(n).keys());
+};
 
 export default function Sidebar() {
-  const createChat = (): void => {};
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [inputUsername, setEmail] = useState("");
+  const [loadingCreateChat, setLoadingCreateChat] = useState(false);
+  const { user, username } = useUserDetails();
+  const toast = useToast();
+  const { signOut } = useAuth();
+
+  const userChatRef = chatCollection().where(
+    "users",
+    "array-contains",
+    username
+  );
+  const [chatSnapshot] = useCollection(userChatRef as any);
+
+  const checkChatExists = () =>
+    !!chatSnapshot?.docs.find(
+      (chat) =>
+        chat.data().users.find((_username) => _username === inputUsername)
+          ?.length > 0
+    );
+
+  const checkUser = async (_username) => {
+    return await userCollection()
+      .where("username", "==", _username)
+      .limit(1)
+      .get();
+  };
+
+  const checkUserDoesNotExist = async () => {
+    const users = await checkUser(inputUsername);
+    const userList = [];
+    users.forEach((doc) => {
+      userList.push(doc.data());
+    });
+    return userList.length === 0;
+  };
+
+  const createChat = async () => {
+    setLoadingCreateChat(true);
+    const userDoesNotExists = await checkUserDoesNotExist();
+    const chatExists = checkChatExists();
+    if (userDoesNotExists) {
+      setEmail("");
+      toast({
+        title: "User not found",
+        description: `User with ${inputUsername} is not available`,
+        status: "error",
+        isClosable: true,
+        duration: 10000,
+      });
+    } else if (chatExists) {
+      setEmail("");
+      toast({
+        title: "Chat exists",
+        description: `Chat with ${inputUsername} already exists`,
+        status: "error",
+        isClosable: true,
+        duration: 10000,
+      });
+    } else {
+      chatCollection()
+        .add({ users: [username, inputUsername] })
+        .then();
+    }
+    setLoadingCreateChat(false);
+  };
+
   return (
-    <Box>
-      <Header>
-        <Avatar
-          cursor={"pointer"}
-          transition={"all 0.25s"}
-          _hover={{ opacity: 0.7 }}
-        />
-        <Flex gap={"1rem"}>
-          <IconButton aria-label={"Chat"}>
-            <IoChatboxEllipsesOutline />
-          </IconButton>
-          <Menu>
-            <MenuButton as={Button}>
-              <IoEllipsisVerticalOutline />
-            </MenuButton>
-            <MenuList>
-              <MenuGroup title="Profile">
-                <MenuItem>My Account</MenuItem>
-                <MenuItem>Payments </MenuItem>
-              </MenuGroup>
-              <MenuDivider />
-              <MenuGroup title="Help">
-                <MenuItem>Docs</MenuItem>
-                <MenuItem>FAQ</MenuItem>
-              </MenuGroup>
-            </MenuList>
-          </Menu>
+    <>
+      <Stack
+        className={"sidebar"}
+        display={"flex"}
+        flexDirection={"column"}
+        spacing={5}
+        p={"1rem 2rem"}
+        maxH={"100vh"}
+        height={"100vh"}
+      >
+        <Flex
+          position={"sticky"}
+          top={"0"}
+          background={"#fff"}
+          zIndex={1}
+          justifyContent={"space-between"}
+          alignItems={"center"}
+          padding={"0.5rem 0"}
+          border-bottom={"1px solid #f7f7f7"}
+        >
+          <Flex gap={"10px"} alignItems={"center"}>
+            <Avatar
+              cursor={"pointer"}
+              transition={"all 0.25s"}
+              _hover={{ opacity: 0.7 }}
+              src={user.photoURL}
+            />
+            <Text color={"gray.600"}>{username}</Text>
+          </Flex>
+          <Flex gap={"1rem"}>
+            <IconButton aria-label={"Chat"}>
+              <IoChatboxEllipsesOutline />
+            </IconButton>
+            <Menu>
+              <MenuButton as={Button}>
+                <IoEllipsisVerticalOutline />
+              </MenuButton>
+              <MenuList>
+                <MenuGroup title="Profile">
+                  <MenuItem>My Account</MenuItem>
+                  <MenuItem>Preferences </MenuItem>
+                </MenuGroup>
+                <MenuDivider />
+                <MenuGroup>
+                  <MenuItem>Settings</MenuItem>
+                  <MenuItem onClick={signOut}>Logout</MenuItem>
+                </MenuGroup>
+              </MenuList>
+            </Menu>
+          </Flex>
         </Flex>
-      </Header>
-      <Stack spacing={5}>
-        <InputGroup size={"md"}>
-          <InputLeftElement bg={"transparent"} pointerEvents="none">
-            <IoSearchOutline color={"gray.300"} />
-          </InputLeftElement>
-          <Input placeholder={"Search"} />
-        </InputGroup>
-        <Button onClick={createChat}>New Chat +</Button>
+        <Stack spacing={5}>
+          <InputGroup size={"md"}>
+            <InputLeftElement bg={"transparent"} pointerEvents="none">
+              <IoSearchOutline color={"gray.300"} />
+            </InputLeftElement>
+            <Input placeholder={"Search"} />
+          </InputGroup>
+          <Button onClick={onOpen}>New Chat +</Button>
+        </Stack>
+        <Stack
+          spacing={5}
+          overflowY={"scroll"}
+          display={"flex"}
+          flexDirection={"column"}
+          flex={"auto"}
+        >
+          {chatSnapshot?.docs.map((chat) => (
+            <Contact key={chat.id} chat={chat} username={username} />
+          ))}
+        </Stack>
       </Stack>
-    </Box>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Start chat </ModalHeader>
+          <ModalCloseButton />
+          <ModalBody pb={6}>
+            <FormControl>
+              <FormLabel>Email</FormLabel>
+              <Input
+                value={inputUsername}
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
+                placeholder="Email Address"
+              />
+            </FormControl>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button
+              onClick={createChat}
+              disabled={!inputUsername || inputUsername === username}
+              colorScheme="blue"
+              isLoading={loadingCreateChat}
+              type={"submit"}
+              mr={3}
+            >
+              Chat
+            </Button>
+            <Button onClick={onClose}>Cancel</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
   );
 }
