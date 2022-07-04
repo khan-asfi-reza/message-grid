e47b91f9c0e9fdac73583c2f0576b859ebe748df
Khan Asfi Reza <khanasfireza10@gmail.com>
2022-07-04 23:12:12 +0600

    :sparkles: Contact to open chat window

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -2,8 +2,10 @@ import { Avatar, Flex, Text } from "@chakra-ui/react";
 import { getRecipientUsername } from "../utils";
 import { useCollection } from "react-firebase-hooks/firestore";
 import { userCollection } from "@db/collections";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
-export default function Contact({ chat, username }) {
+export default function Contact({ chat, username, id }) {
   const recipientUsername = getRecipientUsername(chat.data().users, username);
   const userCollectionRef = userCollection().where(
     "username",
@@ -12,8 +14,20 @@ export default function Contact({ chat, username }) {
   );
   const [recipientSnapshot] = useCollection(userCollectionRef as any);
 
+  const router = useRouter();
+
+  const onClickContact = async () => {
+    await router.push(`/chat/${id}`);
+  };
+
   return (
-    <Flex alignItems={"center"} gap={"1rem"} key={chat.id} cursor={"pointer"}>
+    <Flex
+      onClick={onClickContact}
+      alignItems={"center"}
+      gap={"1rem"}
+      key={chat.id}
+      cursor={"pointer"}
+    >
       <Avatar
         name={recipientUsername}
         src={recipientSnapshot?.docs[0].data().photoURL}
