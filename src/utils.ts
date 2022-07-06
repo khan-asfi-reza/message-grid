7ca9d801a158506c8c15f13f3c97cf47f03941dd
Khan Asfi Reza <khanasfireza10@gmail.com>
2022-07-06 21:28:21 +0600

    :art: Utility function

--- src/utils.ts
+++ src/utils.ts
@@ -1,2 +1,5 @@
 export const getRecipientUsername = (users, userLoggedIn) =>
   users?.filter((toFilter) => toFilter !== userLoggedIn)[0];
+
+export const getQueryId = (router) =>
+  typeof router.query.id === "string" ? router.query.id : router.query.id[0];
