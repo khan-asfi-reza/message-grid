a4cf2c1667c3a5ef02685a08060bd2db9481fb26
Khan Asfi Reza <khanasfireza10@gmail.com>
2022-07-07 03:22:12 +0600

    :sparkles: Message order update

--- src/utils.ts
+++ src/utils.ts
@@ -3,3 +3,14 @@ export const getRecipientUsername = (users, userLoggedIn) =>
 
 export const getQueryId = (router) =>
   typeof router.query.id === "string" ? router.query.id : router.query.id[0];
+
+export const isInViewport = (element) => {
+  const rect = element.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+};
