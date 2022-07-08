a09c863dfe918b005232534c7b44511d66d7d447
Khan Asfi Reza <khanasfireza10@gmail.com>
2022-07-09 01:15:07 +0600

    :art: Code documentation fix

--- src/utils.ts
+++ src/utils.ts
@@ -1,10 +1,33 @@
-export const getRecipientUsername = (users, userLoggedIn) =>
-  users?.filter((toFilter) => toFilter !== userLoggedIn)[0];
+import { DocumentData, QuerySnapshot } from "@firebase/firestore";
+import { NextRouter } from "next/router";
 
-export const getQueryId = (router) =>
+/**
+ * Returns the partner username
+ * @param users List of users in the array
+ * @param userLoggedIn User logged into the client
+ */
+export const getRecipientUsername = (
+  users: Array<string>,
+  userLoggedIn: string
+): string => users?.filter((toFilter) => toFilter !== userLoggedIn)[0];
+
+/**
+ *
+ * @param { NextRouter } router NextJS Router Context
+ * @example
+ * const router = useRouter();
+ * // Your Code
+ * query = getQueryId(router);
+ */
+export const getQueryId = (router: NextRouter) =>
   typeof router.query.id === "string" ? router.query.id : router.query.id[0];
 
-export const isInViewport = (element) => {
+/**
+ * Checks if an HTML Element is in viewport
+ * @param { HTMLElement } element HTML Element
+ * @return { boolean } If element is in viewport
+ */
+export const isInViewport = (element: HTMLElement): boolean => {
   const rect = element.getBoundingClientRect();
   return (
     rect.top >= 0 &&
@@ -14,3 +37,16 @@ export const isInViewport = (element) => {
     rect.right <= (window.innerWidth || document.documentElement.clientWidth)
   );
 };
+
+/**
+ * Gets data from snapshot
+ * Selects the first element from the list
+ * If element is not available in the object or in the list
+ * Then return null
+ * @param {QuerySnapshot<DocumentData>} snapshot Firebase snapshot
+ * @param {string} key Object Key String / Object Key String
+ * @example
+ * getSnapshotData(messageSnapshot, "message")
+ */
+export const getSnapshotData = (snapshot: QuerySnapshot, key: string) =>
+  snapshot?.docs[0]?.data()[key];
